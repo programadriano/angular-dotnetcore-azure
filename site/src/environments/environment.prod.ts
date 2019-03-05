07b2ba0161fd3782aa68ef578587e59989abb9d5
Thiagtao da Silva Adriano <tadriano@band.com.br>
2019-03-05 16:31:03 -0300

    add

--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:5000/api'
+  url: 'http://apieverisdemo.westus.azurecontainer.io/api'
 };
--- site/src/environments/environment.ts
+++ site/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:5000/api'
+  url: 'http://apieverisdemo.westus.azurecontainer.io/api'
 };
 
 /*
